3916ae4314bbd9f4cb93cb21b2626cd36c7d5a65
Alexia Aldea <alexia.elena.aldea@gmail.com>
2026-08-20 23:26:39 +0300

    no message

--- src/app.ts
+++ src/app.ts
@@ -15,9 +15,14 @@ export async function createApp() {
   );
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
-    credentials: true,
-  });
+  origin: [
+    'https://alerta-nationala-web.vercel.app/',
+    'http://localhost:5173',
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
 
   await app.init();
 
